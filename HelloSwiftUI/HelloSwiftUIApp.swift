883c0155f9ffa442ff52e441bfa7a34e246d6469
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-10-04 07:58:35 -0500

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Juan Pablo Lasprilla Correa on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
