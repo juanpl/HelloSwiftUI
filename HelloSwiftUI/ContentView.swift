56194a1f806c06f013e1a0a29159dad364e3d7e6
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-10-04 08:49:54 -0500

    Basic things were learned about SwiftUI such as adding texts, modifying them, adding images to assets and URLs and modifying them.

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack(spacing: 20){
+            Image("ironMan")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 25.0, style: .continuous))
+            
+            AsyncImage(url: URL(string: "https://static.vecteezy.com/system/resources/thumbnails/030/032/289/small_2x/iron-man-mask-portrait-logo-tattoo-poster-pixel-art-illustration-voxel-graphic-photo.jpg"))
+            { image in
+                image.resizable()
+            } placeholder: {
+                Text("Downloading ...")
+            }
+
+            
+            Text("First line")
+                .foregroundStyle(.cyan)
+                .font(.title)
+                .padding([.top], 20)
+            Text("Second line")
+                .foregroundStyle(.green)
+            Text("Thrid Line")
+            HStack{
+                Text("Left")
+                Text("Right")
+                    .fontWeight(.heavy)
+            }
+        }.foregroundStyle(.orange)
+            .padding()
     }
 }
 
